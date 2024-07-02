e2e7c5d6683f59d3dfb2f6f01376387e3e131346
IT21388934 <it21388934@my.sliit.lk>
2024-07-02 16:09:24 +0530

    init

---
+++ InsuranceReport/Controllers/InsuranceReportController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InsuranceReport.DAL;
+
+namespace InsuranceReport.Controllers
+{
+    public class InsuranceReportController : Controller
+    {
+        // GET: InsuranceReport
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult checkNic(string nic)
+        {
+            string result = "null";
+
+            result = InsuranceModule.CheckTheNICType(nic);
+
+            return Json(new { result }, JsonRequestBehavior.AllowGet);
+
+        }
+
+
+    }
+}
---
+++ InsuranceReport/DAL/InsuranceModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InsuranceReport.DAL
+{
+    public class InsuranceModule
+    {
+
+        public static string CheckTheNICType(string nic)
+        {
+
+            string NicType = null;
+
+            if(nic.Length == 12)
+            {
+                NicType = "New Nic";
+            } else if(nic.Length == 10)
+            {
+                NicType = "Old NIC";
+            }
+
+            return NicType;
+
+        }
+    }
+}
